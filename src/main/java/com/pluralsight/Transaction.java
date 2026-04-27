445e9921722470fcdd6e1538f80135d524a871ec
michalhagos <mhayele3@gmail.com>
2026-04-27 12:34:42 -0500

    created my first Class named Transaction. It has 5 fields. I created constructor to help me create new objects. also getters and setters to help me update or access the private fields in the class

---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,66 @@
+package com.pluralsight;
+// created a transaction Class that have 5 fields / variables on it
+public class Transaction {
+    private String date;
+
+    private String time;
+
+    private String  description;
+
+    private String vendor;
+
+    private double amount;
+
+    // created a constructor to help me set the initial values when I create transaction objects
+    public Transaction(String date, String time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+
+
+    }
+    // created getters and setters so that I am able to access my private fields when I create or update new fields in an object
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+
+}
